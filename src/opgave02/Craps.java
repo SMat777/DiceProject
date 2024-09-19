46dd39e5d53b413aa6d27294980aed90a1bc46be
Simon Mathiasen <simon.mathiasen@hotmail.com>
2024-09-19 21:53:52 +0200

    opdateret udgave

--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -5,19 +5,18 @@ import com.sun.tools.javac.Main;
 import java.awt.*;
 import java.util.Scanner;
 
-// 01) Lav en klasse kaldt Craps
+
 public class Craps {
 
     public static int SumOfDice = 0;
-    public static int point = 0;
 
 
-    //Main method
     public static void main(String[] args) {
 
 
         playTwoDice();
         playCraps();
+
     }
 
     private static void playTwoDice() {
@@ -28,13 +27,13 @@ public class Craps {
 
     public static int[] playCraps() {
         Scanner scanner = new Scanner(System.in);
-        // to terninger tilgives et tilfældigt værdig mellem 1 0g 6 og gemmes i et array, derefter lægges sammen
-        int[] diceArray = new int[2];
 
-        diceArray[0] = (int) (Math.random() * 6 + 1);
-        diceArray[1] = (int) (Math.random() * 6 + 1);
+        int[] dice = new int[2];
 
-        SumOfDice = diceArray[0] + diceArray[1];
+        dice[0] = (int) (Math.random() * 6 + 1);
+        dice[1] = (int) (Math.random() * 6 + 1);
+
+        SumOfDice = dice[0] + dice[1];
 
         if (SumOfDice == 7 || SumOfDice == 11) {
             System.out.println("Du slog " + SumOfDice +  " og vandt!");
@@ -50,25 +49,26 @@ public class Craps {
 
         }
 
-        return diceArray;
+        return dice;
     }
 
-    public static boolean rollForPoint(int point) {
+    public static boolean rollForPoint(int diceOne) {
 
-        int[] newRollArray = new int[2];
+        int[] newRolldice = new int[2];
+        int newpoint;
 
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
-            newRollArray[0] = (int) (Math.random() * 6 + 1);
-            newRollArray[1] = (int) (Math.random() * 6 + 1);
-            int newPoint = newRollArray[0] + newRollArray[1];
+            newRolldice[0] = (int) (Math.random() * 6 + 1);
+            newRolldice[1] = (int) (Math.random() * 6 + 1);
+            int newPoint = newRolldice[0] + newRolldice[1];
 
             if (newPoint == 7) {
                 System.out.println("Øv!" + " Du har desværre slået " + newPoint + " og taber spillet!");
                 return false;
-            } else if (newPoint == point) {
-                System.out.println("Tillykke, du har slået " + point + " og vinder!!");
+            } else if (newPoint == diceOne) {
+                System.out.println("Tillykke, du har slået " + diceOne + " og vinder!!");
                 return true;
             } else {
                     System.out.println("Du slog " + newPoint + " og skal derfor kaste igen");
